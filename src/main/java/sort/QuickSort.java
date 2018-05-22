767087c65f4bc501878160c17af2c0d83d8ea7b8
idota <idota@idotadeMacBook-Pro.local>
2018-05-22 10:09:56 +0800

    paramName change

--- src/main/java/sort/QuickSort.java
+++ src/main/java/sort/QuickSort.java
@@ -9,42 +9,42 @@ public class QuickSort {
 
 
     /**
-     * @param arr  传入的数组
-     * @param low  数组起始位置
-     * @param high 数组结尾位置
+     * @param arr   传入的数组
+     * @param start 数组起始位置
+     * @param end   数组结尾位置
      */
-    public static void sort(int[] arr, int low, int high) {
-        int l = low;
-        int h = high;
-        int povit = arr[low];
+    public static void qsort(int[] arr, int start, int end) {
+        int s = start;
+        int e = end;
+        int povit = arr[start];
 
-        while (l < h) {
-            while (l < h && arr[h] >= povit)
-                h--;
-            if (l < h) {
-                int temp = arr[h];
-                arr[h] = arr[l];
-                arr[l] = temp;
-                l++;
+        while (s < e) {
+            while (s < e && arr[e] >= povit)
+                e--;
+            if (s < e) {
+                int temp = arr[e];
+                arr[e] = arr[s];
+                arr[s] = temp;
+                s++;
             }
 
-            while (l < h && arr[l] <= povit)
-                l++;
+            while (s < e && arr[s] <= povit)
+                s++;
 
-            if (l < h) {
-                int temp = arr[h];
-                arr[h] = arr[l];
-                arr[l] = temp;
-                h--;
+            if (s < e) {
+                int temp = arr[e];
+                arr[e] = arr[s];
+                arr[s] = temp;
+                e--;
             }
         }
-        if (l > low) sort(arr, low, l - 1);
-        if (h < high) sort(arr, l + 1, high);
+        if (s > start) qsort(arr, start, s - 1);
+        if (e < end) qsort(arr, s + 1, end);
     }
 
     public static void main(String[] args) {
         int[] arr = {14, 12, 23, 4, 5, 6};
-        sort(arr, 0, arr.length - 1);
+        qsort(arr, 0, arr.length - 1);
         for (int i : arr) {
             System.out.println(i);
         }
